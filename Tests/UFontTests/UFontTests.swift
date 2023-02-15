9fd8039c2028522f2c33277f5c9fb01203f6d623
Mikel <mikelcobian@gmail.com>
2023-02-15 15:10:19 +0100

    test(UFont): testUFontBasicInit covered

--- Tests/UFontTests/UFontTests.swift
+++ Tests/UFontTests/UFontTests.swift
@@ -3,5 +3,8 @@ import XCTest
 
 final class UFontTests: XCTestCase {
 
-
+	func testUFontBasicInit(){
+		let letra = UFont.custom("Raleway-ExtraBold", size: 32)
+		XCTAssertEqual(letra,UFont.custom("Raleway-ExtraBold", size: 32))
+	}
 }
