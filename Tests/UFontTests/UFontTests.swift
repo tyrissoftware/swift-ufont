b317c52e8dd0a48de1000e2ec98b3c5966cd4111
Mikel <mikelcobian@gmail.com>
2023-02-16 12:03:50 +0100

    test(UFont): testUFontSystem covered

--- Tests/UFontTests/UFontTests.swift
+++ Tests/UFontTests/UFontTests.swift
@@ -7,4 +7,10 @@ final class UFontTests: XCTestCase {
 		let letra = UFont.custom("Raleway-ExtraBold", size: 32)
 		XCTAssertEqual(letra,UFont.custom("Raleway-ExtraBold", size: 32))
 	}
+	
+	func testUFontSystem(){
+		let letra = UFont.system(size: 32, weight: .ultraLight, design: .serif)
+		
+		XCTAssertEqual(letra,UFont.system(size: 32, weight: .ultraLight, design: .serif))
+	}
 }
